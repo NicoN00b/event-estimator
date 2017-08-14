41bc8f83c1e8d0c8ac9f656030eba29a745fd4f4
nr <nicholas.raethke@gmail.com>
2017-08-13 22:26:54 -0700

    refactored front end to debug to mvp.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,71 +3,52 @@ import Models.Event;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class App {
 
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         boolean programRunning = true;
-        
 
 
-//        try {
-            while (programRunning) {
-                System.out.println("Welcome to Entertainment Excellence Event Estimator" + " You may type '0' to exit. " +
-                        "Please tell us how many people will be attending your event");
-                userInput = bufferedReader.readLine();
-                userGuests = Integer.parseInt(userInput);
-                if (userInput.equals("q")) {
-                    programRunning = false;
-                } else {
-                    //
-                    System.out.println("Which courses will your guests be having, 'mezza', 'shawarma', 'dessert'. Please enter one at a time");
+        while (programRunning) {
+            System.out.println("Welcome to Entertainment Excellence Event Estimator, we look forward to providing you with the most memorable evening.");
+            System.out.println("This program will guide you through our options");
+            System.out.println("Please tell us how many people will be attending your event (numerals only)");
+            int guests = Integer.parseInt(bufferedReader.readLine());
+            if (guests < 1 || guests >= 1001) {
+                System.out.println("We are sorry, you entered an invalid number, we can only accommodate events up 1000 attendees");
+                System.out.println("Please keep us in mind for future events. Goodbye.");
+                programRunning = false;
+            } else {
+                System.out.println("Please choose from one of our fabulous meal options");
+                System.out.println("1) A Mediterranean feast including a complete mezza selection, delectable shawarma and kebabs, and finished off with homemade Lebanese baklava, halva, and marzipan");
+                System.out.println("2) An endless array of freshly prepared Cantonese Dim Sum, Roasted Duck and Wild Mushroom Entrees, and traditional custards for Dessert");
+                System.out.println("3) New American Comfort and Class delivered roasted and mashed roots with wild mushroom gravy, spring green salad, and a succulent rib-eye with a delectable balsamic reduction, finished off with our famous selection of pies.");
+                System.out.println("Please enter the number that corresponds to your choice.");
+                int dinner = Integer.parseInt(bufferedReader.readLine());
+                System.out.println("Your event will include an award winning beverage program. Here are your options (please enter the corresponding number).");
+                System.out.println("1) Our Non-Alcoholic bar includes organic coffees and teas, soft drinks, home-brewed kombucha and kefir water, and espresso service");
+                System.out.println("2) In addition, you can add a selection of local and internationally renowned craft beers  and fine wines");
+                System.out.println("3) And finally, you may add vintage and original cocktails prepared by master mixologists.");
+                int drinks = Integer.parseInt(bufferedReader.readLine());
+                System.out.println("We work with some of the most talented entertainers in the industry and provide remarkable value due the volume of business we bring to our talent (Again, please enter the corresponding number to your selection or 0 if you do not require entertainment).");
+                System.out.println("1) Our portfolio of Djs can match your tastes perfectly.");
+                System.out.println("2) For truly classy affair, book one of our brilliant pianists.");
+                System.out.println("3) Or for a party that won't stop, pick from one of our lively bands.");
+                int entertainment = Integer.parseInt(bufferedReader.readLine());
+                System.out.println("We offer discounts to make our services more accessible.  If you are a student, please enter 1.  If this event is for a non-profit, please enter 2.  Otherwise, enter 0.");
+                int discount = Integer.parseInt(bufferedReader.readLine());
 
-                    userInput = bufferedReader.readLine();
-                }
-                    if (userInput.equals("q")) {
-                        programRunning = false;
-                    } else {
-                        userDinnerMenu.add(userInput);
-                        System.out.println("Which beverages will your guests be having, 'beer and wine', 'spirits', 'nonalcoholic'");
-                        userInput = bufferedReader.readLine();
-                    }
-                        if (userInput.equals("q")) {
-                            programRunning = false;
-                        } else {
-                            userLibations.add(userInput);
-                            System.out.println("Which entertainment will your guests be enjoying, 'DJ', 'String Quartet', 'Band'");
-                            userInput = bufferedReader.readLine();
-                        }
-                            if (userInput.equals("q")) {
-                                programRunning = false;
-                            } else {
-                                userEntertainment.add(userInput);
-                            }
-
-
-                Event userEvent = new Event(userGuests, userDinnerMenu, userLibations, userEntertainment);
+                Event userEvent = new Event(guests, dinner, drinks, entertainment, discount);
 
 
                 System.out.println("Your estimated cost is $" + userEvent.giveEstimate());
-                System.out.println("Is your party for your 'nonprofit' or are you a 'student'");
-                userInput = bufferedReader.readLine();
-                if (userInput.equals("nonprofit")) {
-                    System.out.println("Your new estimate is " + userEvent.nonProfit());
-                } else if (userInput.equals("student")) {
-                    System.out.println("Your new estimate is " + userEvent.student());
-                } else
-                    programRunning = false;
+                break;
             }
-//        }
-//        catch (IOException e) {
-//            e.printStackTrace();
-//        }
-
+        }
     }
 }
--- src/test/java/Models/EventTest.java
+++ src/test/java/Models/EventTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class EventTest {
     @Test
     public void newEventObjectCorrectlyCreated_true() throws Exception {
-        Event testEvent = new Event(0, 0, 0, 0, 0 );
+        Event testEvent = new Event(0, 0, 0, 0, 0);
         assertEquals(true, testEvent instanceof Event);
     }
 
@@ -52,54 +52,4 @@ public class EventTest {
         Event newEvent = new Event(1, 1, 1, 1, 1);
         assertEquals(67, newEvent.giveEstimate());
     }
-//    @Test
-//    public void newGuest_returnDinnerChoice() throws Exception {
-//        int testGuests = 1;
-//        List<String> testDinnerMenu = new ArrayList<>();
-//        testDinnerMenu.add("mezza sampler");
-//        List<String> testLibations = new ArrayList<>();
-//        List<String> testEntertainment = new ArrayList<>();
-//        Event testEvent = new Event(testGuests, testDinnerMenu, testLibations, testEntertainment);
-//        assertEquals(testDinnerMenu, testEvent.getDinnerMenu());
-//    }
-//    @Test
-//    public void newGuest_returnLibationChoice() throws Exception {
-//        int testGuests = 1;
-//        List<String> testDinnerMenu = new ArrayList<>();
-//        testDinnerMenu.add("mezza sampler");
-//        List<String> testLibations = new ArrayList<>();
-//        testLibations.add("Spirits Bar");
-//        List<String> testEntertainment = new ArrayList<>();
-//        Event testEvent = new Event(testGuests, testDinnerMenu, testLibations, testEntertainment);
-//        assertEquals(testLibations, testEvent.getLibations());
-//    }
-//    @Test
-//    public void newGuest_returnEntertainmentChoice() throws Exception {
-//        int testGuests = 1;
-//        List<String> testDinnerMenu = new ArrayList<>();
-//        testDinnerMenu.add("mezza sampler");
-//        List<String> testLibations = new ArrayList<>();
-//        testLibations.add("Spirits Bar");
-//        List<String> testEntertainment = new ArrayList<>();
-//        testEntertainment.add("String Quartet");
-//        Event testEvent = new Event(testGuests, testDinnerMenu, testLibations, testEntertainment);
-//        assertEquals(testEntertainment, testEvent.getEntertainment());
-//    }
-//    //This test or code preceeding it broke everything :(
-//    @Test
-//    public void newCost_giveEstimate() throws Exception {
-//        int testGuests = 1;
-//        List<String> testDinnerMenu = new ArrayList<>();
-//        testDinnerMenu.add("mezza sampler");
-//        List<String> testLibations = new ArrayList<>();
-//        testLibations.add("Spirits Bar");
-//        List<String> testEntertainment = new ArrayList<>();
-//        testEntertainment.add("String Quartet");
-//        Event testEvent = new Event(testGuests, testDinnerMenu, testLibations, testEntertainment);
-//        double expected = 222.98;
-//        assertEquals(228.98, testEvent.giveEstimate());
-//    }
-//
-//
-//
 }
